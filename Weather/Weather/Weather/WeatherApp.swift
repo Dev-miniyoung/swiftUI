f04caa4c4aa1e3333cb4f8dc78c6b0887b592d2d
YoungMin <miniyoung37@gmail.com>
2021-12-04 22:54:56 +0900

    # WIP Weather UI study

---
+++ Weather/Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Youngmin on 2021/12/04.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
